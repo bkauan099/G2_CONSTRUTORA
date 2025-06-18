762b1926cdbd499db92ad5974ebf95b7ff5be5b5
zlucskywalker <manoel.lucas@discente.ufma.br>
2025-06-18 12:35:23 -0300

    Inserção das páginas de visualização do Cliente e alteração no App.jsx e Login.jsx

---
+++ codigo/Projeto/Sistema de Vistoria/src/pages/HomeCliente/Home.jsx
@@ -0,0 +1,63 @@
+import "./HomeCliente.css";
+import { useNavigate } from "react-router-dom"; 
+
+function Home({ onLogout }) {
+  const navigate = useNavigate(); 
+
+  return (
+    <div className="home-container">
+      
+      <header className="navbar">
+        <div className="logo">CIVIS</div>
+        <nav className="nav-links">
+          <a href="#" onClick={() => navigate("/Home")}>Home</a>
+          <a href="#" onClick={() => navigate("./pages/HomeCliente/AgendarVistoria")}>Agendar Vistoria</a> 
+          <a href="#" onClick={() => navigate("./pages/HomeCliente/MinhasVistorias")}>Minhas Vistorias</a> 
+          <a href="#" onClick={() => navigate("./pages/HomeCliente/MeuImovel")}>Meu Imóvel</a> 
+        </nav>
+        <button className="logout-button" onClick={onLogout}>
+          Sair
+        </button>
+      </header>
+
+      
+      <main className="main-content">
+        <div className="texto">
+          <h1>
+            Bem-vindo ao <br /> <span>CIVIS Cliente</span>
+          </h1>
+          <p>
+            Gerencie seus imóveis e acompanhe suas vistorias de forma rápida, prática e
+            eficiente.
+          </p>
+        </div>
+
+        <div className="imagem">
+          <img src="/imagens/vistoria.png" alt="Imagem Vistoria" />
+        </div>
+      </main>
+
+     
+      <section className="atalhos">
+        <h2>Acesso Rápido</h2>
+        <div className="atalhos-cards">
+          <div className="card" onClick={() => navigate("/agendar-vistoria")}>
+            <img src="/imagens/nova-vistoria.png" alt="Agendar Vistoria" />
+            <h3>Agendar Vistoria</h3>
+          </div>
+          <div className="card" onClick={() => navigate("/minhas-vistorias")}>
+            <img src="/imagens/agendadas.png" alt="Minhas Vistorias" />
+            <h3>Minhas Vistorias</h3>
+          </div>
+          <div className="card" onClick={() => navigate("/meus-imoveis")}>
+            <img src="/imagens/imoveis.png" alt="Meus Imóveis" />
+            <h3>Meus Imóveis</h3>
+          </div>
+          
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Home;
